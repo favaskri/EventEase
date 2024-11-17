bd6c490152cfaab0e8548aeee18ba52d9e582170
favaskri <favas13270@gmail.com>
2024-11-17 07:53:35 +0530

    total_ticket_price adding using custom tag done

--- EventEase/tickets/models.py
+++ EventEase/tickets/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from events.models import Event
+from django.utils.timezone import now
+
 
 # Create your models here.
 class Ticket(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    
     ticket_type = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity_available = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2,null=False)
+    quantity_available = models.PositiveIntegerField(null=True, blank=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2,default=0.00)
+    created_at = models.DateTimeField(default=now)
+
 
     def __str__(self):
         return f"{self.ticket_type} - {self.event.title}"
--- EventEase/tickets/urls.py
+++ EventEase/tickets/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     
     path('tickets/', views.tickets, name='tickets'),
     path('purchase_tickets/<int:pk>/', views.purchase_tickets, name='purchase_tickets'),
+    path('success/<int:ticket_id>/', views.ticket_success, name='ticket_success')
     
 ]
